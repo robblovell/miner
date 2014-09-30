4d36d69513eebe75b78c81c22f6a37a6dd72c657
Robb Lovell <robbl@moj.io>
2014-09-30 12:33:22 -0700

    compatible with production.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-#Sidekiq.configure_server do |config|
-#  config.redis = { :url => 'redis://54.210.187.172:6379', :namespace => 'sidekiq' }
-#end
-#
-#Sidekiq.configure_client do |config|
-#  config.redis = { :url => 'redis://54.210.187.172:6379', :namespace => 'sidekiq' }
-#end
+if (ENV["RAILS_ENV"] == "production")
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => 'redis://54.210.187.172:6379', :namespace => 'sidekiq' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => 'redis://54.210.187.172:6379', :namespace => 'sidekiq' }
+  end
+end
